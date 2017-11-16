81737890dadee8f6a93242ff14a3713a0b3b84f0
张诗健 <zsjcwy_xcode@163.com>
2017-11-16 11:09:12 +0800

    commit

---
+++ UIViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewDemo
+//
+//  Created by 讯心科技 on 2017/11/16.
+//  Copyright © 2017年 讯心科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewDemo
+//
+//  Created by 讯心科技 on 2017/11/16.
+//  Copyright © 2017年 讯心科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
